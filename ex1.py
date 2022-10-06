3ab97cdd9672c22c420a02e64f5d8b95c4a53aed
yury <yuriy-t87@yandex.ru>
2022-10-06 19:04:41 +0300

    Первая задача

---
+++ ex1.py
@@ -0,0 +1,17 @@
+# Вычислить число c заданной точностью d
+# Пример:
+# при $d = 0.001, π = 3.141.$ $10^{-1} ≤ d ≤10^{-10}$
+import math
+
+
+def get_pi(d):
+    ndigits = 0
+    
+    while d < 1:
+        d *= 10
+        ndigits += 1
+    return round(math.pi, ndigits)
+
+
+num = float(input('Введите точность числа Пи, например, 0.001: '))
+print(f'π = {get_pi(num)}')        
